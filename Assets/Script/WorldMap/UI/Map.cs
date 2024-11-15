3b99b8ce50bd6db7a79a2585f6dbd0dfc44baf5d
cju4409 <cj4409@naver.com>
2024-11-15 13:44:37 +0900

    추가: Map 스크립트

---
+++ Assets/Script/WorldMap/UI/Map.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CheonJiWoon {
+    public class Map : MonoBehaviour
+    {
+        public Camera mapCamera;
+        public Transform view;
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
+
+
